265cf23a19b58882e8ac0d785dc9f9eb82649789
l2ujiuggl <l2ujiuggl@gmail.com>
2019-06-18 15:29:10 +0800

    new

---
+++ MyApplication/app/src/main/java/com/hemly/myapplication/ServiceActivity.java
@@ -0,0 +1,31 @@
+package com.hemly.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.adjust.sdk.Adjust;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+        Adjust.appWillOpenUrl(data, getApplicationContext());
+    }
+
+    public void onServiceClick(View v) {
+        Intent intent = new Intent(this, ServiceExample.class);
+        startService(intent);
+    }
+
+    public void onReturnClick(View v) {
+        finish();
+    }
+}
